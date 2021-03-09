8b0eeb7893e80cc74b34c24f7a140e9469f6cc3f
Nyári, Dávid Tamás <dev@dnyari.info>
2021-03-09 22:57:21 +0100

    Finished documentation for TransportMethod

--- src/core/transport/mod.rs
+++ src/core/transport/mod.rs
@@ -1,3 +1,5 @@
+//! # Transport core module
+//! Module containing interface and type definitions for transport methods that can be implemented in RustyMQ 
 pub mod network;
 
 pub use self::network::NetworkAddress;
@@ -6,29 +8,50 @@ use core::message::{RawMessage, PeerId};
 use core::socket::{SocketError, OpFlag, PeerIdentification};
 use std::collections::{HashSet};
 
+
+/// # TransportMethod
+/// TransportMethod should contain the data for a transport layer to be able to establish a connection
 #[derive(Debug)]
 pub enum TransportMethod
 {
+    /// Connect through network
     Network(NetworkAddress),
+    /// Dummy value, should not be handled
     Dummy
 }
 
+/// # Transport
+/// Trait that every transport layer implementation has to implement. This is the interface a Socket uses internally
+/// to establis a connection
 pub trait Transport: Send + Sync {
+    /// Send message
     fn send(&mut self, message: RawMessage, flags: OpFlag) -> Result<(), SocketError>;
+    /// Receive message
     fn receive(&mut self, flags: OpFlag) -> Result<RawMessage, (Option<PeerId>, SocketError)>;
+    /// Close connection to the peer identified by [`PeerIdentification`]
     fn close_connection(&mut self, peer_identification: PeerIdentification) -> Result<Option<PeerId>, SocketError>;
+    /// Query the [`PeerId`]s of all the connected peers
     fn query_connected_peers(&self) -> HashSet<PeerId>;
+    /// Close the connection to all connected peers
     fn close(self) -> Result<(), SocketError>;
 }
 
+/// # InitiatorTransport
+/// Trait that every transport layer has to implement that can establish a connection
 pub trait InitiatorTransport : Transport {
+    /// Establist connection to peer defined by TransportMethod
     fn connect(&mut self, target: TransportMethod) -> Result<Option<PeerId>, SocketError>;
 }
 
+/// # AcceptorTransport
+/// Trait that every transport layer has to implement that can listen for connections
 pub trait AcceptorTransport : Transport {
+    /// Listen for connections from peers on the TransportMethod defined
     fn bind(&mut self, target: TransportMethod) -> Result<Option<PeerId>, SocketError>;
 }
 
+/// # BidirectionalTransport
+/// Trait that every transport layer has to implenent that can both establish connections and listen for them
 pub trait BidirectionalTransport : InitiatorTransport + AcceptorTransport {
     
 }
