a79d02aa5de1e9b6bd5c0820a2662d01827c4695
Nyári, Dávid Tamás <dev@dnyari.info>
2021-03-09 22:43:40 +0100

    Documented socket module

--- src/core/socket.rs
+++ src/core/socket.rs
@@ -1,42 +1,68 @@
+//! # Socket module
+//! This module contains the traits serving as the main interaction point for RustyMQ
+
 use core::message::{Message, RawMessage, TypedMessage, PeerId, MessageMetadata, Buffer};
 use core::transport::{TransportMethod};
 
 use std::convert::{TryFrom, TryInto, From};
 
+/// # Operation flags
+/// Configuration for individual send and receive calls on [`InwardSocket`]s and [`OutwardSocket`]s
 #[derive(Debug)]
 #[derive(Clone)]
 pub enum OpFlag {
+    /// Default operation mode
     Default,
-    NoWait,
-//    Timeout(Duration)
+    /// Do not wait for operation to finish. Asychronous mode
+    NoWait
 }
 
+/// # [`Socket`] errors
+/// Contains the possible errors for a socket that may need to be handled on the user side 
 #[derive(Debug)]
 #[derive(Clone)]
 pub enum SocketError {
+    /// Signals if the socket is not in the correct state to process the requested operation
     IncorrectStateError,
+    /// The specified peer identifier in [`MessageMetadata`] is unkown by the 
     UnrelatedPeer,
+    /// The specified conversation in the [`MessageMetadata`] is not known by the socket
     UnrelatedConversation,
+    /// The specified conversation part in the [`MessageMetadata`] is not known by the socket
     UnrelatedConversationPart,
+    /// The target peer identifier is not specified in the [`MessageMetadata`] and cannot be inferred
     UnknownPeer,
+    /// The specific [`TransportMethod`] is already used by the socket
     TransportMethodAlreadyInUse,
+    /// The specific [`TransportMethod`] cannot be found, cannot be connected to
     TransportTargetUnreachable,
+    /// The tharget peer specified in [`MessageMetadata`] disconnected
     Disconnected,
+    /// The tharget specified in [`TransportMethod`] refused the connection
     ConnectionRefused,
-    UnknownDataFormatReceived,
+    /// The operation timed out
     Timeout,
+    /// The connected peer has an incompatible version or communication model
     IncompatiblePeer,
+    /// Handshake with the target peer failed
     HandshakeFailed,
+    /// The [`OpFlag`] specified for the [`InwardSocket`] or [`OutwardSocket`] operation is not supprted by 
+    /// the socket or the underlieing Transport
     UnsupportedOpFlag(OpFlag),
+    /// DNS domain name required but not available in [`core::network:;NetworkAddress`] 
     MissingDNSDomain,
+    /// The [`TransportMethod`] is not supported by the Transport
     InvalidTransportMethod,
+    /// The connection already exists
     AlreadyConnected,
-    AlreadyInUse,
+    /// The requested operation is not supported by socket
     NotSupportedOperation,
-    CouldNotConnect,
-    AlreadyDisconnected
+    /// Could not connect to address provided by [`TransportMethod`] 
+    CouldNotConnect
 }
 
+/// # [`Socket`] errors
+/// Contains the possible errors for a socket that may need to be handled internally or by the user 
 #[derive(Debug)]
 #[derive(Clone)]
 pub enum SocketInternalError {
@@ -49,7 +75,6 @@ pub enum SocketInternalError {
     TransportTargetUnreachable,
     Disconnected,
     ConnectionRefused,
-    UnknownDataFormatReceived,
     Timeout,
     IncompatiblePeer,
     HandshakeFailed,
@@ -57,16 +82,16 @@ pub enum SocketInternalError {
     MissingDNSDomain,
     InvalidTransportMethod,
     AlreadyConnected,
-    AlreadyInUse,
     NotSupportedOperation,
     CouldNotConnect,
-    AlreadyDisconnected,
 
     IncompleteData,
-    UnknownInternalError
+    UnknownInternalError,
+    UnknownDataFormatReceived,
 }
 
 impl SocketInternalError {
+    /// Create user [`SocketError`] from internal error. Will panic in case of error is not handleable by user
     pub fn externalize_result<T>(result: Result<T, SocketInternalError>)-> Result<T, SocketError> {
         result.map_err(|x| {SocketError::from(x)})
     }
@@ -74,6 +99,7 @@ impl SocketInternalError {
 
 impl From<SocketInternalError> for SocketError
 {
+    /// Convert [`SocketInternalError`] to [`SocketError`] . Will panic in case of error is not handleable by user
     fn from(input: SocketInternalError) -> SocketError {
         match input {
             SocketInternalError::IncorrectStateError => SocketError::IncorrectStateError,
@@ -85,7 +111,6 @@ impl From<SocketInternalError> for SocketError
             SocketInternalError::TransportTargetUnreachable => SocketError::TransportTargetUnreachable,
             SocketInternalError::Disconnected => SocketError::Disconnected,
             SocketInternalError::ConnectionRefused => SocketError::ConnectionRefused,
-            SocketInternalError::UnknownDataFormatReceived => SocketError::UnknownDataFormatReceived,
             SocketInternalError::Timeout => SocketError::Timeout,
             SocketInternalError::IncompatiblePeer => SocketError::IncompatiblePeer,
             SocketInternalError::HandshakeFailed => SocketError::HandshakeFailed,
@@ -93,32 +118,40 @@ impl From<SocketInternalError> for SocketError
             SocketInternalError::MissingDNSDomain => SocketError::MissingDNSDomain,
             SocketInternalError::InvalidTransportMethod => SocketError::InvalidTransportMethod,
             SocketInternalError::AlreadyConnected => SocketError::AlreadyConnected,
-            SocketInternalError::AlreadyInUse => SocketError::AlreadyInUse,
             SocketInternalError::NotSupportedOperation => SocketError::NotSupportedOperation,
             SocketInternalError::CouldNotConnect => SocketError::CouldNotConnect,
-            SocketInternalError::AlreadyDisconnected => SocketError::AlreadyDisconnected,
             err => panic!("SocketInternalError occured that cannot be converted to SocketError: {:?}", err)
         }
     }
 }
 
+/// Identifier for a peer
 pub enum PeerIdentification {
+    /// Identify by the peer identifier in [`MessageMetadata`]
     PeerId(PeerId),
+    /// Identify by the original [`TransportMethod`]
     TransportMethod(TransportMethod)
 }
 
+/// Result type for a typed sending operation
 #[derive(Debug)]
 pub enum SendTypedError<T> where T: TryInto<Buffer>, Buffer: TryInto<T>, <T as TryInto<Buffer>>::Error : std::fmt::Debug {
+    /// Issue was related to the socket during the sending operation
     Socket(SocketError),
+    /// Issue happened during the serialization of the typed value
     Conversion(<RawMessage as TryFrom<TypedMessage<T>>>::Error)
 }
 
+/// Result type for a typed receive operation
 #[derive(Debug)]
 pub enum ReceiveTypedError<T> where T: TryInto<Buffer>, Buffer: TryInto<T>, <Buffer as TryInto<T>>::Error : std::fmt::Debug {
+    /// Issue was related to the socked during the receiving operation
     Socket((Option<PeerId>, SocketError)),
+    /// Issue happened during the deserialization of the received typed value
     Conversion(<TypedMessage<T> as TryFrom<RawMessage>>::Error)
 }
 
+/// Result type for a typed query operation (a send operation followed by a received operation)
 #[derive(Debug)]
 pub enum QueryTypedError<T> 
     where T: TryInto<Buffer>,
@@ -126,20 +159,37 @@ pub enum QueryTypedError<T>
           <T as TryInto<Buffer>>::Error : std::fmt::Debug,
           <Buffer as TryInto<T>>::Error : std::fmt::Debug
 {
+    /// Issue happened during the sending operation. For details please see [`SendTypedError`]
     Send(SendTypedError<T>),
+    /// Issue happened during the receiveing operation. For details please see [`ReceiveTypedError`]
     Receive(ReceiveTypedError<T>)
 }
 
+/// # General **Socket** trait
+/// The main interface for the user to interact with during operaton
+/// There can be several implementations of it dependinng on communication model
 pub trait Socket : Send + Sync {
+    /// Connect to a new peer specified by the parameter. The result can contain the PeerId which should be used 
+    /// in [`MessageMetadata`] in future transactions to identify the peer
     fn connect(&mut self, target: TransportMethod) -> Result<Option<PeerId>, SocketError>;
+    /// Bind to an interface to listen for connections given by the first parameter. The result can contain a PeerId that will contain
+    /// the self identifier PeerId.
     fn bind(&mut self, target: TransportMethod) -> Result<Option<PeerId>, SocketError>;
+    /// Close connection to a peer specified by [`PeerIdentification`]
     fn close_connection(&mut self, peer_identification: PeerIdentification) -> Result<(), SocketError>;
+    /// Close the socket with all its connections
     fn close(self) -> Result<(), SocketError>;
 }
- 
+
+
+/// # Outward **Socket** trait
+/// The main interface for the user to send messages through the opened RustyMQ socket
+/// There can be several implementations of it depending on the communication model used
 pub trait OutwardSocket : Socket {
+    /// Send a raw message
     fn send(&mut self, message:RawMessage, flags:OpFlag) -> Result<MessageMetadata, SocketError>;
 
+    /// Send a typed message. Requires T to be covertable to the [`RawMessage`] type
     fn send_typed<T>(&mut self, message:TypedMessage<T>, flags: OpFlag) -> Result<MessageMetadata, SendTypedError<T>>
         where T:TryInto<Buffer> + TryFrom<Buffer>,
               <T as TryInto<Buffer>>::Error: std::fmt::Debug 
@@ -154,9 +204,14 @@ pub trait OutwardSocket : Socket {
     }
 }
 
+/// # Inward **Socket** trait
+/// The main interface for the user to receive messages through the opened RustyMQ socket
+/// There can be several implementations of it depending on the communication model used
 pub trait InwardSocket : Socket {
+    /// Receive a raw message
     fn receive(&mut self, flags:OpFlag) -> Result<RawMessage, (Option<PeerId>, SocketError)>;
 
+    /// Receive a typed message. Requires [`RawMessage`] to be convertible into T
     fn receive_typed<T>(&mut self, flags:OpFlag) -> Result<TypedMessage<T>, ReceiveTypedError<T>>
         where T:TryInto<Buffer> + TryFrom<Buffer>,
               <T as TryFrom<Buffer>>::Error: std::fmt::Debug 
@@ -171,13 +226,18 @@ pub trait InwardSocket : Socket {
     }
 }
 
+/// # Bidirectional **Socket** trait
+/// The main interface for the user to send and receive messages through the same opened RustyMQ socket
+/// There can be several implementations of it depending on the communication model used
 pub trait BidirectionalSocket: OutwardSocket + InwardSocket
 {
+    /// Execute a query (send and then receive) with a raw message
     fn query(&mut self, message :RawMessage, flags:OpFlag) -> Result<RawMessage, (Option<PeerId>, SocketError)> {
         self.send(message, flags.clone()).map_err(|error| {(None, error)})?;
         self.receive(flags)
     }
 
+    /// Execute a query (send and then receive) with a typed message. Requires T to be convertibe to and from RawMessage
     fn query_typed<T>(&mut self, message:TypedMessage<T>, flags:OpFlag) -> Result<TypedMessage<T>, QueryTypedError<T>>
         where T:TryInto<Buffer> + TryFrom<Buffer>,
               <T as TryInto<Buffer>>::Error: std::fmt::Debug,
@@ -192,6 +252,7 @@ pub trait BidirectionalSocket: OutwardSocket + InwardSocket
         }
     }
 
+    /// Respont to a query (receive and then reply) with a raw message
     fn respond<T: Fn(RawMessage) -> RawMessage> (&mut self, flags:OpFlag, processor: T) -> Result<(), (Option<PeerId>, SocketError)> {
         let query = self.receive(flags.clone())?;
         let query_metadata = query.metadata().clone();
@@ -200,6 +261,7 @@ pub trait BidirectionalSocket: OutwardSocket + InwardSocket
         Ok(())
     }
 
+    /// Respond to a query (receive and then reply) with a typed message. Requires T to be convertibe to and from RawMessage
     fn respond_typed<T, Q: Fn(TypedMessage<T>) -> TypedMessage<T>>(&mut self, flags:OpFlag, processor: Q) -> Result<(), QueryTypedError<T>>
         where T:TryInto<Buffer> + TryFrom<Buffer>,
               <T as TryInto<Buffer>>::Error: std::fmt::Debug,
